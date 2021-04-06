204f03525ff9094d55c8ea54bc24752c66353e31
Michaela Williams <michaela.williams359@gmail.com>
2021-04-06 13:43:50 -0400

    Item now calculates Item.Price on construction. Changed Item.Ingredients to Item.ItemRecipe.

--- BakeryShop/Shop/Item.cs
+++ BakeryShop/Shop/Item.cs
@@ -5,12 +5,28 @@ namespace BakeryShop
     {
         public string Name { get; private set; }
         public double Price { get; private set; }
-        public Recipe Ingredients { get; private set; }
+        public Recipe ItemRecipe { get; private set; }
 
         public Item(string name)
         {
             this.Name = name;
+            CalculatePrice();
+        }
 
+        // Calculates price based on the cost of ingredients
+        private void CalculatePrice()
+        {
+            Price = 0;
+
+            (Ingredient, float)[] ingredients = ItemRecipe.Ingredients;
+            foreach((Ingredient, float) ingredient in ingredients)
+            {
+                var ingredientCost = ingredient.Item1.Cost;
+                var ingredientQuantity = ingredient.Item2;
+                Price += ingredientCost * ingredientQuantity;
+            }
+
+        
         }
 
     }
